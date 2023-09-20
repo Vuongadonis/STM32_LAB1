fe4f21a319660b74bd7c5da5d14cef911752aad2
vuong <vuong03062003@gmail.com>
2023-09-20 10:31:07 +0700

    update ex_2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -144,40 +144,40 @@ void TurnLED4() {
   void clearNumberOnClock(int num) {
 	  switch(num) {
 	  case 0:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, SET);
 		  break;
 	  case 1:
 		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
 		  break;
 	  case 2:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
 		  break;
 	  case 3:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
 		  break;
 	  case 4:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
 		  break;
 	  case 5:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
 		  break;
 	  case 6:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
 		  break;
 	  case 7:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
 		  break;
 	  case 8:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
 		  break;
 	  case 9:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
 		  break;
 	  case 10:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
 		  break;
 	  case 11:
-		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
 		  break;
 	  default:
 		  clearAllClock();
@@ -255,7 +255,7 @@ void TurnLED4() {
 		  setNumberOnClock(countSecond/5);
 		  setNumberOnClock(countMinute/5);
 		  setNumberOnClock(countHour/5);
-		  time_counter = 100;
+		  time_counter = 10;
 	  }
 	  HAL_Delay(DELAY);
     /* USER CODE END WHILE */
